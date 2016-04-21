f053e4318045cd8b982ce2ab298bd73fc9e9d12c
Matt Phillips <mattphillips1016@gmail.com>
2016-04-21 20:57:12 +0100

    added resize function to reset sidebar height

--- js/mattphillips/sidebar.js
+++ js/mattphillips/sidebar.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    $('.sidebar').css({
-        height: ($(document).height()) +'px'
-    });
+    setSidebarHeight();
 });
+
+$( window ).resize(function() {
+    setSidebarHeight();
+});
+
+function setSidebarHeight() {
+    $('.sidebar').css({height: ($(document).height()) +'px'});
+}
