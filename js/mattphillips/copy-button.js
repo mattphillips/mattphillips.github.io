91f754ea44c180c42dc7aa7c2a632fe4d7234fd3
Matt Phillips <mattphillips1016@gmail.com>
2016-04-22 17:54:23 +0100

    disabled copy button from displaying on smaller devices

--- js/mattphillips/copy-button.js
+++ js/mattphillips/copy-button.js
@@ -1,17 +1,19 @@
-const COPY_BUTTON = '<div class="col-md-1"><button type="button" class="btn btn-primary glyphicon glyphicon-copy copy-button"></button></div>';
-const CODE_BLOCK_WIDTH = 'col-md-11';
+const COPY_BUTTON = '<div class="col-md-1 col-sm-1 col-xs-1"><button type="button" class="btn btn-primary glyphicon glyphicon-copy copy-button"></button></div>';
+const CODE_BLOCK_WIDTH = 'col-md-11 col-sm-11 col-xs-10';
 const ROW_ID_PREFIX = 'code-row-';
 const CODE_BLOCK_CLASS = '.highlighter-rouge'
 
 $(function() {
-  $(CODE_BLOCK_CLASS).each(function(i) {
-    addCopyButtonToCodeBlock(this, i);
-  });
+  if ($(window).width() > 992) {
+    $(CODE_BLOCK_CLASS).each(function(i) {
+      addCopyButtonToCodeBlock(this, i);
+    });
 
-  $('.copy-button').click(function() {
-    var $codeBlock = extractCodeBlock(this);
-    copyCodeToClipboard($codeBlock.text());
-  });
+    $('.copy-button').click(function() {
+      var $codeBlock = extractCodeBlock(this);
+      copyCodeToClipboard($codeBlock.text());
+    });
+  }
 });
 
 function addCopyButtonToCodeBlock(block, id) {
